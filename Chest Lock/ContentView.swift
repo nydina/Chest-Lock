8b6d55870d91d8aa95c026753198b83ceb1ea533
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-11-20 15:22:41 +0100

    Add styles to button

--- Chest Lock/ContentView.swift
+++ Chest Lock/ContentView.swift
@@ -3,27 +3,38 @@ import LocalAuthentication
 
 struct ContentView: View {
     @State private var isAppLocked = true
-
+    
     var body: some View {
         VStack {
-            if isAppLocked {
-                Button("Open") {
+            Text(isAppLocked ? "Application locked" : "Application unlocked")
+            ZStack {
+                Circle().frame(maxWidth: 135).foregroundColor(isAppLocked ? .red : .green)
+                Circle().frame(maxWidth: 130).foregroundColor(.white)
+                Image(systemName: isAppLocked ? "lock" :"lock.open").font(.largeTitle)
+            }
+            
+            Button(action: {
+                if isAppLocked {
                     authenticate()
-                }
-            } else {
-                Button("Lock") {
+                } else {
                     isAppLocked = true
                 }
+            }) {
+                Text(isAppLocked ? "Open" : "Lock")
+                    .padding()
+                    .foregroundColor(.white)
             }
+            .background(isAppLocked ? Color.green : Color.red)
+            .cornerRadius(6)
+            .padding()
         }
-        .padding()
     }
 
     func authenticate() {
         let context = LAContext()
-
+        
         var error: NSError?
-
+        
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             let reason = "Authenticate to unlock the app"
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
