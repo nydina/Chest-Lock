3b6cae50456941f8cd02bf22a49358a09618801a
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-11-20 14:02:06 +0100

    Initial Commit

---
+++ Chest Lock/Chest_LockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chest_LockApp.swift
+//  Chest Lock
+//
+//  Created by Dina RAZAFINDRATSIRA on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chest_LockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
